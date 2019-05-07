cfdcb230f018e98e291aa6a1573bb8e5ecf398d4
Arm <chayanin.suatap@gmail.com>
2019-05-07 14:30:25 +0700

    add no blue

--- predict.py
+++ predict.py
@@ -4,14 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 print('loading model')
-model = load_model('model.hdf5')
+model = load_model('trained_model/model_ep-4.hdf5')
 print('loaded')
-img = scipy.misc.imread('my_tower.jpg', mode='RGB').astype(np.float)
+img = scipy.misc.imread('my_input.jpg', mode='RGB').astype(np.float)
 img = scipy.misc.imresize(img, (256,256))
 xs = np.array([img])/127.5 - 1.
 print('xs shape', xs.shape)
 pred = model.predict( xs)
-# print(pred.shape)
+# print(np.array(pred).shape)
+print(pred)
 plt.imshow(pred[1][0] * 0.5 + 0.5)
 plt.show()
 
