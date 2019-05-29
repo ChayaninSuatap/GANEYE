0c3c3877e4f453a7c613218443ce78aa047c1842
Arm <chayanin.suatap@gmail.com>
2019-05-29 13:40:52 +0700

    save

--- predict.py
+++ predict.py
@@ -2,11 +2,13 @@ from keras.models import load_model
 import scipy
 import matplotlib.pyplot as plt
 import numpy as np
+from pix2pix_no_blue import Pix2Pix
 
 print('loading model')
-model = load_model('saved_model/model_no_blue_ep-0-sample_no-200.hdf5')
+model = load_model('saved_model/model_no_blue_ep-4-sample_no-0.hdf5')
+
 print('loaded')
-img = scipy.misc.imread('my_input.jpg', mode='RGB').astype(np.float)
+img = scipy.misc.imread('my_input_2.jpg', mode='RGB').astype(np.float)
 img = scipy.misc.imresize(img, (256,256))
 xs = np.array([img])/127.5 - 1.
 print('xs shape', xs.shape)
