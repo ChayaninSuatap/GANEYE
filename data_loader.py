39047bf6a60154f7389e16979ac05a54f25f9969
Arm <chayanin.suatap@gmail.com>
2019-05-29 16:22:42 +0700

    ** use pilutil directly

--- data_loader.py
+++ data_loader.py
@@ -4,6 +4,7 @@ from PIL import Image
 from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
+import pilutil
 
 class DataLoader():
     def __init__(self, dataset_name, img_res=(128, 128)):
@@ -28,8 +29,8 @@ class DataLoader():
             _w = int(w/2)
             img_A, img_B = img[:, :_w, :], img[:, _w:, :]
 
-            img_A = scipy.misc.imresize(img_A, self.img_res)
-            img_B = scipy.misc.imresize(img_B, self.img_res)
+            img_A = pilutil.imresize(img_A, self.img_res)
+            img_B = pilutil.imresize(img_B, self.img_res)
 
             # If training => do random flip
             if not is_testing and np.random.random() < 0.5:
@@ -54,14 +55,14 @@ class DataLoader():
             batch = path[i*batch_size:(i+1)*batch_size]
             imgs_A, imgs_B = [], []
             for img in batch:
-                img = self.imread(img)
+                img = pilutil.imread(img)
                 h, w, _ = img.shape
                 half_w = int(w/2)
                 img_A = img[:, :half_w, :]
                 img_B = img[:, half_w:, :]
 
-                img_A = scipy.misc.imresize(img_A, self.img_res)
-                img_B = scipy.misc.imresize(img_B, self.img_res)
+                img_A = pilutil.imresize(img_A, self.img_res)
+                img_B = pilutil.imresize(img_B, self.img_res)
 
                 if not is_testing and np.random.random() > 0.5:
                         img_A = np.fliplr(img_A)
@@ -77,60 +78,4 @@ class DataLoader():
 
 
     def imread(self, path):
-        im = Image.open(path)
-        return fromimage(im, mode='RGB').astype(np.float)
-        # return scipy.misc.imread(path, mode='RGB').astype(np.float)
-
-def fromimage(im, flatten=False, mode=None):
-    """
-    Return a copy of a PIL image as a numpy array.
-
-    This function is only available if Python Imaging Library (PIL) is installed.
-
-    Parameters
-    ----------
-    im : PIL image
-        Input image.
-    flatten : bool
-        If true, convert the output to grey-scale.
-    mode : str, optional
-        Mode to convert image to, e.g. ``'RGB'``.  See the Notes of the
-        `imread` docstring for more details.
-
-    Returns
-    -------
-    fromimage : ndarray
-        The different colour bands/channels are stored in the
-        third dimension, such that a grey-image is MxN, an
-        RGB-image MxNx3 and an RGBA-image MxNx4.
-
-    """
-    if not Image.isImageType(im):
-        raise TypeError("Input is not a PIL image.")
-
-    if mode is not None:
-        if mode != im.mode:
-            im = im.convert(mode)
-    elif im.mode == 'P':
-        # Mode 'P' means there is an indexed "palette".  If we leave the mode
-        # as 'P', then when we do `a = array(im)` below, `a` will be a 2-D
-        # containing the indices into the palette, and not a 3-D array
-        # containing the RGB or RGBA values.
-        if 'transparency' in im.info:
-            im = im.convert('RGBA')
-        else:
-            im = im.convert('RGB')
-
-    if flatten:
-        im = im.convert('F')
-    elif im.mode == '1':
-        # Workaround for crash in PIL. When im is 1-bit, the call array(im)
-        # can cause a seg. fault, or generate garbage. See
-        # https://github.com/scipy/scipy/issues/2138 and
-        # https://github.com/python-pillow/Pillow/issues/350.
-        #
-        # This converts im from a 1-bit image to an 8-bit image.
-        im = im.convert('L')
-
-    a = np.array(im)
-    return a
+        return pilutil.imread(path, mode='RGB').astype(np.float)
