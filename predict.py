0ed019a179d335344589f6e03cd9cd0975978dcd
Arm <chayanin.suatap@gmail.com>
2019-06-06 20:53:37 +0700

    predict ceye

--- predict.py
+++ predict.py
@@ -8,13 +8,13 @@ print('loading model')
 o = Pix2Pix(gen_weights_fn='gen.hdf5', dis_weights_fn='dis.hdf5', load_for_predict=True, save_path='saved_model_eyes')
 model = o.combined
 print('loaded')
-#sample image ( compare with validate while training )
-# o.sample_images(9999,9999)
 
 #predict 
 img = scipy.misc.imread('input.jpg', mode='RGB').astype(np.float)
 img = scipy.misc.imresize(img, (256,256))
 xs = np.array([img])/127.5 - 1.
+xs = o.make_imgb_with_label(xs,[1])
+
 print('xs shape', xs.shape)
 pred = model.predict( xs)
 # print(np.array(pred).shape)
